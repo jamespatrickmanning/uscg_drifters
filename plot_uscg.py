77c92b130dc4ad12d16f9f1f65a391169f245692
Jim Manning <james.manning@noaa.gov>
2020-06-10 12:53:49 -0400

    another draft plot_uscg for Jack to followup

--- plot_uscg.py
+++ plot_uscg.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun  10 11:39:35 2020
-
 @author: JiM
 Routine to plot USCG iSLMDB drifters in our area after downloading a csv file from Linc,
 where we selected units from the output of "get_uscg_for_our_area.py"
 where our_area is 34-46N, and -77--64W (ie the Northeast Shelf)
-
 """
 
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+os.environ['PROJ_LIB'] = 'c:\\Users\\Joann\\anaconda3\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\share'
 from mpl_toolkits.basemap import Basemap
 
-area='NEC'
-input_file='c:\\users\\DELL\\Downloads\\all_assets_637272425998242740.csv'
+area='NE'
+input_file='all_assets_637272425998242740.csv'
 
 def getgbox(area):
   # gets geographic box based on area
@@ -33,31 +35,34 @@ def getgbox(area):
     gbox=[-70.75,-70.,41.7,42.23] # CCBAY
   elif area=='NEC':
     gbox=[-69.,-64.,39.,43.5] # NE Channel
+  elif area=='NE':
+    gbox=[-76.,-64.,35.,46.] # NE Channel  
   return gbox
 
 def make_basemap(gb):
-    m = Basemap(projection='stere',lon_0=(gb[0]+gb[1])/2.,lat_0=(gb[2]+gb[3])/2.,lat_ts=0,llcrnrlat=gb[2],urcrnrlat=gb[3],\
-                llcrnrlon=gb[0],urcrnrlon=gb[1],rsphere=6371200.,resolution='f',area_thresh=100)# JiM changed resolution to "c" for crude
-    # draw coastlines, state and country boundaries, edge of map.
+    m = Basemap(projection='merc',lon_0=(gb[0]+gb[1])/2.,lat_0=(gb[2]+gb[3])/2.,lat_ts=0,llcrnrlat=gb[2],urcrnrlat=gb[3],\
+                llcrnrlon=gb[0],urcrnrlon=gb[1],rsphere=6371200.,resolution='l',area_thresh=100)# JiM changed resolution to "c" for crude
+    # draw coastlines
     m.drawcoastlines()
-    m.drawstates()
-    m.drawcountries()
-    parallels = np.arange(0.,90,1.)
-    m.drawparallels(parallels,labels=[1,0,0,0],fontsize=20)
+    parallels = np.arange(0.,90,3.)
+    m.drawparallels(parallels,labels=[1,0,0,0],fontsize=14,linewidth=0)
     # draw meridians
-    meridians = np.arange(180.,360.,1.)
-    m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=20)
-    # draw filled contours.
-    dept_clevs=[50,100, 150,300,1000]
-    dept_cs=m.contour(x,y,depth,dept_clevs,colors='black')
-    plt.clabel(dept_cs, inline = True, fontsize =15,fmt="%1.0f")
+    meridians = np.arange(180.,360.,3.)
+    m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=14,linewidth=0)
     return m
 
 # Make a basemap
 gb=getgbox(area)
-make_basemap(gb)
+m=make_basemap(gb)
 df=pd.read_csv(input_file)
-x,y=m(df['LONGITUDE'].values,df[' LATITUDE'].values)
-plot(x,y,'.')
+#df=df[' LATITUDE'].replace('',np.nan,inplace=True)
+#df=df.dropna(subset=[' LATITUDE'], inplace=True)
+df=df[~df[' LATITUDE'].str.contains('LATITUDE')]
+df[' LATITUDE']=pd.to_numeric(df[' LATITUDE'],downcast="float")
+df[' LONGITUDE']=pd.to_numeric(df[' LONGITUDE'],downcast="float")
+x,y=m(df[' LONGITUDE'].values,df[' LATITUDE'].values)
+plt.plot(x,y,'r.',markersize=4)
+plt.title('USCG drifters May 2019 - May 2020')
 
-    
+
+
