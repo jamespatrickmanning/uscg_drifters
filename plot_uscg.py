f8bfbbfa7de8cc24ddef4829e393a5fee3b3a209
Jim Manning <james.manning@noaa.gov>
2020-06-10 16:03:52 -0400

    Add files via upload

--- plot_uscg.py
+++ plot_uscg.py
@@ -10,12 +10,18 @@ where our_area is 34-46N, and -77--64W (ie the Northeast Shelf)
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+#NOTE: I needed the following two lines on my Toshiba laptop runs
 import os
 os.environ['PROJ_LIB'] = 'c:\\Users\\Joann\\anaconda3\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\share'
 from mpl_toolkits.basemap import Basemap
+from scipy.interpolate import griddata
 
 area='NE'
 input_file='all_assets_637272425998242740.csv'
+time_period='May 2019 - May 2020'
+gs=50      # number of bins in the x and y direction so,  if you want more detail, make it bigger
+ss=100     # subsample input data so, if you want more detail, make it smaller
+cont=[-200]
 
 def getgbox(area):
   # gets geographic box based on area
@@ -36,33 +42,51 @@ def getgbox(area):
   elif area=='NEC':
     gbox=[-69.,-64.,39.,43.5] # NE Channel
   elif area=='NE':
-    gbox=[-76.,-64.,35.,46.] # NE Channel  
+    gbox=[-76.,-66.,35.,44.5] # NE Shelf 
   return gbox
-
+def add_isobath(m,gs,ss,cont):
+    # draws an isobath on map given gridsize,subsample rate,and contour level
+    # these inputs are typically 50, 100, and 200 for entire shelf low resolution
+    url='http://apdrc.soest.hawaii.edu/erddap/griddap/hawaii_soest_794e_6df2_6381.csv?b_bathy[(0000-01-01T00:00:00Z):1:(0000-01-01T00:00:00Z)][(35.0):1:(45.0)][(-76.0):1:(-66.0)]'
+    df=pd.read_csv(url)
+    df=df.drop('time',axis=1)
+    df=df[1:].astype('float')# removes unit row and make all float          
+    Xb,Yb=m.makegrid(gs,gs) # where "gs" is the gridsize specified in hardcode section
+    Xb,Yb=m(Xb,Yb) # converts grid to basemap coordinates .
+    xlo,yla=m(df['longitude'][0:-1:ss].values,df['latitude'][0:-1:ss].values)
+    zi = griddata((xlo,yla),df['b_bathy'][0:-1:ss].values,(Xb,Yb),method='linear')
+    CS=m.contour(Xb,Yb,zi,cont,zorder=0,color='k',linewidths=[.3],linestyles=['dashed'])
+    plt.clabel(CS, inline=1, fontsize=8,fmt='%d')
 def make_basemap(gb):
     m = Basemap(projection='merc',lon_0=(gb[0]+gb[1])/2.,lat_0=(gb[2]+gb[3])/2.,lat_ts=0,llcrnrlat=gb[2],urcrnrlat=gb[3],\
-                llcrnrlon=gb[0],urcrnrlon=gb[1],rsphere=6371200.,resolution='l',area_thresh=100)# JiM changed resolution to "c" for crude
+                llcrnrlon=gb[0],urcrnrlon=gb[1],rsphere=6371200.,resolution='l',area_thresh=100,suppress_ticks=True)# JiM changed resolution to "c" for crude
     # draw coastlines
-    m.drawcoastlines()
+    #m.drawcoastlines()
+    m.fillcontinents(color='grey',lake_color='grey')
     parallels = np.arange(0.,90,3.)
-    m.drawparallels(parallels,labels=[1,0,0,0],fontsize=14,linewidth=0)
+    m.drawparallels(parallels,labels=[1,0,0,0],fontsize=10,linewidth=0)
     # draw meridians
     meridians = np.arange(180.,360.,3.)
-    m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=14,linewidth=0)
+    m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=10,linewidth=0)
     return m
 
 # Make a basemap
 gb=getgbox(area)
-m=make_basemap(gb)
+m=make_basemap(gb)        # adds coastline
+print('adding isobath which takes a minute ... comment out to save time')
+add_isobath(m,gs,ss,cont) # adds isobath
 df=pd.read_csv(input_file)
 #df=df[' LATITUDE'].replace('',np.nan,inplace=True)
 #df=df.dropna(subset=[' LATITUDE'], inplace=True)
-df=df[~df[' LATITUDE'].str.contains('LATITUDE')]
-df[' LATITUDE']=pd.to_numeric(df[' LATITUDE'],downcast="float")
+df=df[~df[' LATITUDE'].str.contains('LATITUDE')] # gets rid of extra header lines
+df[' LATITUDE']=pd.to_numeric(df[' LATITUDE'],downcast="float") # converts string to float
 df[' LONGITUDE']=pd.to_numeric(df[' LONGITUDE'],downcast="float")
-x,y=m(df[' LONGITUDE'].values,df[' LATITUDE'].values)
-plt.plot(x,y,'r.',markersize=4)
-plt.title('USCG drifters May 2019 - May 2020')
+drifter_ids=list(np.unique(df['Asset Id'].values))
+for k in drifter_ids:
+    df1=df[df['Asset Id']==k]
+    x,y=m(df1[' LONGITUDE'].values,df1[' LATITUDE'].values) # converts to basemap coordinates
+    plt.plot(x,y,'-')#,markersize=4)
+plt.title('USCG drifters '+time_period)
 
 
 
